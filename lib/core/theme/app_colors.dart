5fe63c9fafa4a804111c7c34d44aabd321842f8a
mohamed <bodyalhoseny@gmail.com>
2026-09-11 21:04:25 +0300

    create App color

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,39 @@
+
+
+// import 'dart:ui';
+import 'package:flutter/material.dart';
+class AppColors {
+  // primary color
+  static const Color primary = Color(0xff000000);
+  static const Color primaryLight = Color(0xff333333);
+  static const Color primaryDark = Color(0xff000000);
+
+  // secondary color
+  static const Color secondary = Color(0xff666666);
+  static const Color secondaryLight = Color(0xff999999);
+  static const Color secondaryDark = Color(0xff444444);
+
+  // Accent color
+  static const Color accent = Color(0xffffffff);
+  static const Color success = Color(0xff000000);
+  static const Color warning = Color(0xff000000);
+  static const Color error = Color(0xff000000);
+
+  // Light theme
+  static const Color lightBackground = Color(0xffffffff);
+  static const Color lightSurface = Color(0xfff5f5f5);
+  static const Color lightText = Color(0xff000000);
+  static const Color lightTextSecondary = Color(0xff666666);
+  static const Color lightDivider = Color(0xffe0e0e0);
+
+  // dark theme
+
+  static const Color darkBackground  = Color(0xff000000);
+  static const Color darkSurface  = Color(0xff121212);
+  static const Color darkText  = Color(0xffffffff);
+  static const Color darkTextSecondary  = Color(0xffcccccc);
+  static const Color darkDivider  = Color(0xff333333);
+
+
+
+}
